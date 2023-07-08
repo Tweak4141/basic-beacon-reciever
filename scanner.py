1f147dc666d4fe9f1712f80caf43bf6d8fef4d34
Tweak <ynan243@gmail.com>
2023-07-08 15:50:40 -0600

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,9 +1,30 @@
 import time
 
 from beacontools import BeaconScanner, EddystoneTLMFrame
+from flask import Flask
+from cache import Cache
+devices = Cache()
+app = Flask(__name__)
 
+
+@app.route('/temp')
+def tempInfo():
+    data = devices.getKey(macAddr)
+    temp = data["packet"]
+    return temp
+    
+@app.route('/device')
+def deviceInfo():
+    return devices.getKey(macAddr)
+    
+@app.route('/')
+def info():
+    return 'Sensor Server Running'
+    
 def callback(bt_addr, rssi, packet, additional_info):
     print("<%s, %d> %s %s" % (bt_addr, rssi, packet, additional_info))
+    devices.setKey(bt_addr, { "bt_addr": bt_addr, "rssi": rssi, "packet": packet, "additional_info": additional_info })
+    
 print("Scan Started")
 scanner = BeaconScanner(callback)
 scanner.start()
