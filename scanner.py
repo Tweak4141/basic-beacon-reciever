f226ae1b6b12c2473d7ed201d0544437c5a316f6
Tweak <ynan243@gmail.com>
2023-07-08 16:50:01 -0600

    Fix

--- scanner.py
+++ scanner.py
@@ -10,16 +10,7 @@ app = Flask(__name__)
 seconds = 60
 scanning = False
 
-def restartScan():
-    global scanning
-    if scanning is False:
-        scanner.start()
-        print("Scan Started")
-        scanning = True
-        time.sleep(seconds)
-        scanner.stop()
-        print("Scan Stopped")
-        scanning = False
+
 
 @app.route('/temp/<macAddr>')
 def tempInfo(macAddr):
@@ -50,8 +41,8 @@ def callback(bt_addr, rssi, packet, additional_info):
     devices.setKey(bt_addr, { "bt_addr": bt_addr, "rssi": rssi, "packet": { "tx_pwr": packet.tx_power, "url": packet.url }, "additional_info": additional_info, "date_created": datetime.now().strftime("%m/%d/%Y, %H:%M:%S") })
     
 scanner = BeaconScanner(callback)
-app.run(host="0.0.0.0", port="5956")
 
 while True:
-    restartScan()
+    app.run(host="0.0.0.0", port="5956")
+    scanner.start()
 
