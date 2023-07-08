b494abe3e3231f265689230dd25f9cb0e9be21ce
Tweak <ynan243@gmail.com>
2023-07-08 16:27:51 -0600

    Add date

--- scanner.py
+++ scanner.py
@@ -3,6 +3,8 @@ import time
 from beacontools import BeaconScanner
 from flask import Flask, jsonify
 from cache import Cache
+from datetime import datetime
+
 devices = Cache()
 app = Flask(__name__)
 
@@ -13,7 +15,7 @@ def tempInfo(macAddr):
     if not data:
         return "NO DEVICE FOUND", 404
     temp = data["packet"]["url"]
-    return str(temp.replace("http://",""))
+    return jsonify({ "temp": temp.replace("http://",""), "date": data["date_created"] })
     
 @app.route('/device/<macAddr>')
 def deviceInfo(macAddr):
@@ -27,7 +29,7 @@ def info():
     return 'Sensor Server Running'
     
 def callback(bt_addr, rssi, packet, additional_info):
-    devices.setKey(bt_addr, { "bt_addr": bt_addr, "rssi": rssi, "packet": { "tx_pwr": packet.tx_power, "url": packet.url }, "additional_info": additional_info })
+    devices.setKey(bt_addr, { "bt_addr": bt_addr, "rssi": rssi, "packet": { "tx_pwr": packet.tx_power, "url": packet.url }, "additional_info": additional_info, "date_created": datetime.now().strftime("%m/%d/%Y, %H:%M:%S") })
     
 print("Scan Started")
 scanner = BeaconScanner(callback)
